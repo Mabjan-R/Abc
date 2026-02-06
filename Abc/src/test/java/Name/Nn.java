486f5b86bacb51c31c3ed6d3767c957c1064f51e
Mabjan-R <mabjan89@gmail.com>
2026-02-06 12:29:19 +0530

    sec

--- Abc/src/test/java/Name/Nn.java
+++ Abc/src/test/java/Name/Nn.java
@@ -1,10 +1,13 @@
 package Name;
 
+import org.testng.Assert;
+
 public class Nn {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("hi i am man");
+      Assert.fail("failed intentionally");
 	}
 
 }
